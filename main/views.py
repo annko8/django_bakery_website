49e7470b2064b585437034416dfecebe7506153d
Anna <koannva@gmail.com>
2024-02-13 16:16:25 +0300

    main and about pages

--- main/views.py
+++ main/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
 
 
 def index(request):
-
-    return render(request, "main/index.html")
+    context = {"title": "Main"}
+    return render(request, "main/index.html", context)
 
 
 def about(request):
-    return HttpResponse("About page")
+    return render(request, "main/about.html")
